e54f4895596840fddb177ec9763bb1f1720bfc27
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-06-02 21:27:37 +0300

    complete sentence counter

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
+import re
+
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+            self.value = value
+      
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self,value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+        
+
+    def is_sentence(self):
+        if self.value[-1] == ".":
+            return True
+        else:
+            return False
+    def is_question(self):
+        if self.value[-1] == "?":
+            return True
+        else:
+            return False
+    def is_exclamation(self):
+        if self.value[-1] == "!":
+            return True
+        else:
+            return False
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]+', self.value)
+        sentences = [s.strip() for s in sentences if s.strip()]
+
+        return len(sentences)
+    pass
